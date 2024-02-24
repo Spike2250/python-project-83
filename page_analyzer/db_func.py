284867add3bf01817c0f50e188e54fa8328f286e
Shilov Ilya <shilov.spike2250@yandex.ru>
2024-02-24 14:13:31 +0500

    test fix db_func

--- page_analyzer/db_func.py
+++ page_analyzer/db_func.py
@@ -46,15 +46,22 @@ def find_checks(url_id):
 
 
 def find_all_urls():
-    query = """
-        SELECT urls.id,
-               urls.name,
-               url_checks.created_at,
-               url_checks.status_code
-        FROM urls LEFT JOIN url_checks
-            ON urls.id = url_checks.url_id
-        GROUP BY urls.id, url_checks.status_code
-        ORDER BY urls.id DESC;
-    """
-    return select_from_db(select_query=query,
-                          many_values=True)
+    conn = connect_to_db()
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+        q_select = '''
+            SELECT DISTINCT ON (urls.id)
+                urls.id AS id,
+                urls.name AS name,
+                url_checks.created_at AS last_check,
+                url_checks.status_code AS status_code
+            FROM urls LEFT JOIN url_checks
+                ON urls.id = url_checks.url_id
+            AND url_checks.id = (SELECT MAX(id)
+                FROM url_checks
+                WHERE url_id = urls.id)
+            ORDER BY urls.id DESC;'''
+        cur.execute(q_select)
+        urls = cur.fetchall()
+    conn.close()
+
+    return urls
