96f5f75f48b67a8e9d21a9742acaea5749e048b9
Shilov Ilya <shilov.spike2250@yandex.ru>
2024-03-01 00:23:41 +0500

    add contextmanager db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,39 +1,20 @@
 import os
-# from contextlib import contextmanager
+from contextlib import contextmanager
 from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-# @contextmanager
-# def connection():
-#     conn = psycopg2.connect(DATABASE_URL)
-#     try:
-#         yield conn
-#     finally:
-#         conn.close()
-
-'''
-не смог, к сожалению, разобраться
-    c contextmanager
-использование как:
-
-def func(arg):
-    with connection() as conn:
-        with get_cursor(conn) as cursor:
-            ... do something database ...
-    return result
-
-упорно не проходит hexlet тесты
-буду признателен если подскажите в чем ошибка
-'''
-
-
-def get_connect():
-    return psycopg2.connect(DATABASE_URL)
+@contextmanager
+def connection():
+    database_url = os.getenv("DATABASE_URL")
+    conn = psycopg2.connect(database_url)
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 
 def get_cursor(connection):
@@ -41,91 +22,68 @@ def get_cursor(connection):
 
 
 def find_url_by_id(id_):
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                SELECT *
-                FROM urls
-                WHERE id = %s;"""
-            cursor.execute(query, (id_, ))
-            url = cursor.fetchone()
-    conn.close()
-    return url
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            SELECT *
+            FROM urls
+            WHERE id = %s;"""
+        cursor.execute(query, (id_, ))
+        return cursor.fetchone()
 
 
 def find_url_by_name(name):
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                SELECT *
-                FROM urls
-                WHERE name = %s;"""
-            cursor.execute(query, (name, ))
-            url = cursor.fetchone()
-    conn.close()
-    return url
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            SELECT *
+            FROM urls
+            WHERE name = %s;"""
+        cursor.execute(query, (name, ))
+        return cursor.fetchone()
 
 
 def find_checks(url_id):
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                SELECT *
-                FROM url_checks
-                WHERE url_id = %s
-                ORDER BY id DESC;"""
-            cursor.execute(query, (url_id, ))
-            check = cursor.fetchall()
-    conn.close()
-    return check
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            SELECT *
+            FROM url_checks
+            WHERE url_id = %s
+            ORDER BY id DESC;"""
+        cursor.execute(query, (url_id, ))
+        return cursor.fetchall()
 
 
 def find_all_urls():
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                SELECT DISTINCT ON (urls.id)
-                    urls.id AS id,
-                    urls.name AS name,
-                    url_checks.created_at AS last_check,
-                    url_checks.status_code AS status_code
-                FROM urls LEFT JOIN url_checks
-                    ON urls.id = url_checks.url_id
-                AND url_checks.id = (SELECT MAX(id)
-                    FROM url_checks
-                    WHERE url_id = urls.id)
-                ORDER BY urls.id DESC;"""
-            cursor.execute(query)
-            urls = cursor.fetchall()
-    conn.close()
-    return urls
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            SELECT DISTINCT ON (urls.id)
+                urls.id AS id,
+                urls.name AS name,
+                url_checks.created_at AS last_check,
+                url_checks.status_code AS status_code
+            FROM urls LEFT JOIN url_checks
+                ON urls.id = url_checks.url_id
+            AND url_checks.id = (SELECT MAX(id)
+                FROM url_checks
+                WHERE url_id = urls.id)
+            ORDER BY urls.id DESC;"""
+        cursor.execute(query)
+        return cursor.fetchall()
 
 
 def insert_new_url(data):
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                INSERT INTO urls (name, created_at)
-                VALUES (%s, %s) RETURNING id;"""
-            cursor.execute(query=query, vars=data)
-            url = cursor.fetchone()
-    conn.close()
-    return url
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            INSERT INTO urls (name, created_at)
+            VALUES (%s, %s) RETURNING id;"""
+        cursor.execute(query=query, vars=data)
+        return cursor.fetchone()
 
 
 def insert_url_check(data):
-    conn = get_connect()
-    with conn:
-        with get_cursor(conn) as cursor:
-            query = """
-                INSERT INTO url_checks (
-                    url_id, status_code, h1,
-                    title, description, created_at)\
-                VALUES (%s, %s, %s, %s, %s, %s);"""
-            cursor.execute(query=query, vars=data)
-    conn.close()
+    with connection() as conn, get_cursor(conn) as cursor:
+        query = """
+            INSERT INTO url_checks (
+                url_id, status_code, h1,
+                title, description, created_at)\
+            VALUES (%s, %s, %s, %s, %s, %s);"""
+        cursor.execute(query=query, vars=data)
